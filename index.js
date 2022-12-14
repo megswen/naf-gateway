51ff8a2e043bbe9dbcc271dcebbb212e2d308024
Megan Swenson <mrsmeganswenson@gmail.com>
2022-12-14 10:30:52 -0700

    Repo setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+let port = process.env.PORT || 3002;
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, async() => {
+    try {
+        console.log('Listening on port #' + port);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+module.exports = app;
