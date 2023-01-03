78accda78da7644cef502ddf0e44205fe7d74665
Megan Swenson <mrsmeganswenson@gmail.com>
2023-01-03 15:52:22 -0700

    Added code to function to set partner ID on contact

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,7 +1,30 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const axios = require('axios');
+
 const setHSContactPartnerId = async(partnerId, contactId) => {
     try {
+        const properties = {
+            naflink_partnerid: partnerId
+        }
 
+        let hubspotResponse = await axios({ // API call to HubSpot to set contact's partner ID after creation of a partner in NAFLink
+            url: `https://api.hubapi.com/crm/v3/objects/contacts/${contactId}`,
+            method: 'PATCH',
+            headers: {
+                'Authorization': `Bearer ${process.env.HS_AUTH_TOKEN}`, // Authenticating with access token from private app
+                'Content-Type': 'application/json',
+                'accept': 'application/json'
+            },
+            data: { properties }
+        });
+
+        console.log(hubspotResponse);
     } catch(error) {
         console.log(error);
     }
+}
+
+module.exports = {
+    setHSContactPartnerId
 }
